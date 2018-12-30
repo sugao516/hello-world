40e6796d44ad7873ce144e172a6760a37cb8145d
sugao516 <sugao516.a@gmail.com>
2018-12-30 19:10:32 +0900

    003

--- 003/xsl.js
+++ 003/xsl.js
@@ -19,62 +19,57 @@ function xslFunc(xslSource, xmlSource, params, elem) {
 	}
 }
 
-function DOMDocument() {
+var DOMDocument = function () {
 
 	var isIE = !window.XSLTProcessor;
 	if(!isIE) {
-		this.load = load;
+		this.load = function (url, isXsl) {
+			var request = new XMLHttpRequest();
+			request.open("GET", url, false);
+			request.send(null);
+			return request.responseXML;
+		};
 		this.XSLTProcessor = window.XSLTProcessor;
 	} else {
-		this.load = loadIE;
-		this.XSLTProcessor = XSLTProcessorIE;
+		this.load = function (url, isXsl) {
+			var objType = isXsl ? "Msxml2.FreeThreadedDOMDocument" : "Msxml2.DOMDocument";
+			var doc = new ActiveXObject(objType);
+			doc.async = false;
+			doc.load(url);
+			var docErr = doc.parseError;
+			if (docErr.errorCode != 0) {
+				var msg = "ActiveXObject error !"
+				+ "<br>errorCode: " + docErr.errorCode
+				+ "<br>reason: " + docErr.reason
+				+ "<br>url: " + url
+				+ "<br>objType: " + objType
+				;
+				throw new Error(msg);
+			}
+			return doc;
+		};
+		this.XSLTProcessor = function () {
+
+			this.xslt = new ActiveXObject("Msxml2.XSLTemplate");
+		
+			this.importStylesheet = function(styleSheet) {
+				this.xslt.stylesheet = styleSheet;
+				this.xslProc = this.xslt.createProcessor();
+			}
+		
+			this.setParameter = function(namespaceURI, localName, value) {
+				this.xslProc.addParameter(localName, value);
+			}
+		
+			this.transformToFragment = function(source, owner) {
+				this.xslProc.input = source;
+				this.xslProc.transform();
+				var namespaceURI = owner.documentElement.namespaceURI;
+				var elem = owner.createElementNS(namespaceURI, null);
+				elem.innerHTML = this.xslProc.output;
+				return elem.firstElementChild;
+			}
+		};
 	}
 
-	function load(url, isXsl) {
-		var request = new XMLHttpRequest();
-		request.open("GET", url, false);
-		request.send(null);
-		return request.responseXML;
-	}
-
-	function loadIE(url, isXsl) {
-		var objType = isXsl ? "Msxml2.FreeThreadedDOMDocument" : "Msxml2.DOMDocument";
-		var doc = new ActiveXObject(objType);
-		doc.async = false;
-		doc.load(url);
-		var docErr = doc.parseError;
-		if (docErr.errorCode != 0) {
-			var msg = "ActiveXObject error !"
-			+ "<br>errorCode: " + docErr.errorCode
-			+ "<br>reason: " + docErr.reason
-			+ "<br>url: " + url
-			+ "<br>objType: " + objType
-			;
-			throw new Error(msg);
-		}
-		return doc;
-	}
-}
-
-function XSLTProcessorIE() {
-
-	this.xslt = new ActiveXObject("Msxml2.XSLTemplate");
-
-	this.importStylesheet = function(styleSheet) {
-		this.xslt.stylesheet = styleSheet;
-		this.xslProc = this.xslt.createProcessor();
-	}
-
-	this.setParameter = function(namespaceURI, localName, value) {
-		this.xslProc.addParameter(localName, value);
-	}
-
-	this.transformToFragment = function(source, owner) {
-		this.xslProc.input = source;
-		this.xslProc.transform();
-		var namespaceURI = owner.documentElement.namespaceURI;
-		var elem = owner.createElementNS(namespaceURI, null);
-		elem.innerHTML = this.xslProc.output;
-		return elem.firstElementChild;
-	}
 }
